05c623cb11f190db3af591b8ba6e496091fc1b07
navidmir <irblackhat@yahoo.com>
2022-12-28 19:24:11 +0330

    fix problem for adding commands to crontab files

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,6 @@ if [[ $@ == crond* ]] && [ -z "$CRON_SCHEDULE" ]; then
 fi
 
 # Write cron schedule
-echo "$CRON_SCHEDULE python -u /backup/backup.py > /dev/stdout && rotate-backups-s3 -U $AWS_ACCESS_KEY_ID -P $AWS_SECRET_ACCESS_KEY --aws-host $(echo $S3_ENDPOINT |sed 's/^http\(\|s\):\/\///g') --daily=$ROTATION_DAILY --weekly=$ROTATION_WEEKLY --monthly=$ROTATION_MONTHLY --yearly=$ROTATION_YEARLY $S3_PATH > /dev/stdout" >> /var/spool/cron/crontabs/root
+echo "$CRON_SCHEDULE python -u /backup/backup.py > /dev/stdout && rotate-backups-s3 -U $AWS_ACCESS_KEY_ID -P $AWS_SECRET_ACCESS_KEY --aws-host $(echo $S3_ENDPOINT |sed 's/^http\(\|s\):\/\///g') --daily=$ROTATION_DAILY --weekly=$ROTATION_WEEKLY --monthly=$ROTATION_MONTHLY --yearly=$ROTATION_YEARLY $S3_PATH > /dev/stdout" > /var/spool/cron/crontabs/root
 
 exec "$@"
